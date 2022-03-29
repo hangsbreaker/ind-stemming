1321292060f8ae15f429a8a4b39cec6b0f68d4cd
Hangs Breaker <hangsbreaker@gmail.com>
2022-03-29 14:15:39 +0700

    Delete example.py

--- example.py
+++
@@ -1,9 +0,0 @@
-from Stemming import Stemming
-
-str = "berpuasa adalah kewajiban bagi setiap umat islam di dunia dan bermanfaat untuk akhirat"
-# str = input("Masukkan Kalimat: ")
-# print(str)
-
-s = Stemming()
-res = s.stem(str)
-print(res)
